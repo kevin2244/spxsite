6abeb7c5496c2379e301c53e17824c805fc41e0a
Kevin Smith <12529748+kevin2244@users.noreply.github.com>
2018-04-09 17:12:55 +0100

    Added missing files for previous commit.

---
+++ src/App/src/Handler/ScrappageSchemesExplainedHandlerFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Psr\Container\ContainerInterface;
+use Zend\Expressive\Template\TemplateRendererInterface;
+
+class ScrappageSchemesExplainedHandlerFactory
+{
+    public function __invoke(ContainerInterface $container) : ScrappageSchemesExplainedHandler
+    {
+        $template = $container->get(TemplateRendererInterface ::class);
+        return new ScrappageSchemesExplainedHandler($template);
+    }
+}
